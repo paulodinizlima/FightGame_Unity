501930437b1755da3e1ecaa84620da0f65c95d49
paulodinizlima <diniz_lima@hotmail.com>
2024-06-14 21:33:10 -0300

    Player 1 - Move, Jump e Crouch

---
+++ Assets/My Scripts/Player1Actions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1Actions : MonoBehaviour
+{
+    public float JumpSpeed = 0.5f;
+    public GameObject Player1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void JumUp()
+    {
+        Player1.transform.Translate(0, JumpSpeed, 0);
+    }
+}
--- Assets/My Scripts/Player1Move.cs
+++ Assets/My Scripts/Player1Move.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class Player1Move : MonoBehaviour
 {
+    private Animator Anim;
+    public float WalkSpeed = 0.001f;
+    private bool IsJumping = false;
+    private AnimatorStateInfo Player1Layer0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Anim = GetComponentInChildren<Animator>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
+
+        // Walking left and right
+        if(Player1Layer0.IsTag("Motion"))
+        {
+            if(Input.GetAxis("Horizontal") > 0)
+            {
+                Anim.SetBool("Forward", true);
+                transform.Translate(WalkSpeed,0,0);
+            }
+            if(Input.GetAxis("Horizontal") < 0)
+            {
+                Anim.SetBool("Backward", true);
+                transform.Translate(-WalkSpeed,0,0);
+            }
+        }
+        if(Input.GetAxis("Horizontal") == 0)
+        {
+            Anim.SetBool("Forward", false);
+            Anim.SetBool("Backward", false);
+        }
+        // Jumping and Crouching
+        if(Input.GetAxis("Vertical") > 0)
+        {
+            if(!IsJumping)
+            {
+                IsJumping = true;
+                Anim.SetTrigger("Jump");
+                StartCoroutine(JumpPause());
+            }
+        }
+        if(Input.GetAxis("Vertical") < 0)
+        {
+            Anim.SetBool("Crouch", true);
+        }
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            Anim.SetBool("Crouch", false);
+        }
+    }
+
+    IEnumerator JumpPause()
+    {
+        yield return new WaitForSeconds(1.0f);
+        IsJumping = false;
     }
 }
